967f037d79d023b50441d87027b46fd076419d05
Yuvraj Doshi <yuvraj.doshi@walmart.com>
2025-12-04 09:46:50 +0530

    Refactor

--- include/core/LimitLevel.hpp
+++ include/core/LimitLevel.hpp
@@ -10,8 +10,6 @@ namespace LOB {
         // --- Constructor ---
         explicit LimitLevel(Price price);
 
-        // --- Core Operations ---
-        
         // Appends an order to the end of the list (Time Priority)
         void append(Order* order);
 
